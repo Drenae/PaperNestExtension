2eb71cd26ddfe2f60b98cc6a0f5bbd5bca3777bc
Drenae <adrien.guilpain@gmail.com>
2026-08-01 07:45:52 +0200

    refactor(example): export dialogs page

--- examples/papernestextension_example/src/app/pages/__init__.py
+++ examples/papernestextension_example/src/app/pages/__init__.py
@@ -1,5 +1,6 @@
+from .dialogs_page import DialogsPage
 from .forms_page import FormsPage
 from .home_page import HomePage
 from .pickers_page import PickersPage
 
-__all__ = ["FormsPage", "HomePage", "PickersPage"]
+__all__ = ["DialogsPage", "FormsPage", "HomePage", "PickersPage"]
