ea3bc84726ba177a6baad2fd6bb8d3ebc2caf121
Drenae <adrien.guilpain@gmail.com>
2026-08-01 06:16:17 +0200

    feat: export PaperNestAlertDialog from material controls

--- src/papernestextension/controls/material/__init__.py
+++ src/papernestextension/controls/material/__init__.py
@@ -1,3 +1,7 @@
+from .papernest_alert_dialog import (
+    PaperNestAlertDialog,
+    PaperNestDialogVariant,
+)
 from .papernest_color_picker import PaperNestColorPicker
 from .papernest_glide_rail import (
     PaperNestGlideRail,
