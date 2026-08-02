46540ab37cb0f8eaa1cf27c39fea509ee458b9e0
Drenae <adrien.guilpain@gmail.com>
2026-08-02 15:22:16 +0200

    feat(button): export PaperNestButton from material package

--- src/papernestextension/controls/material/__init__.py
+++ src/papernestextension/controls/material/__init__.py
@@ -2,6 +2,7 @@ from .papernest_alert_dialog import (
     PaperNestAlertDialog,
     PaperNestDialogVariant,
 )
+from .papernest_button import PaperNestButton, PaperNestButtonVariant
 from .papernest_color_picker import PaperNestColorPicker
 from .papernest_glide_rail import (
     PaperNestGlideRail,
@@ -11,3 +12,15 @@ from .papernest_icon_picker import (
     PaperNestIconPicker,
     PaperNestIconPickerOption,
 )
+
+__all__ = [
+    "PaperNestAlertDialog",
+    "PaperNestDialogVariant",
+    "PaperNestButton",
+    "PaperNestButtonVariant",
+    "PaperNestColorPicker",
+    "PaperNestGlideRail",
+    "PaperNestGlideRailDestination",
+    "PaperNestIconPicker",
+    "PaperNestIconPickerOption",
+]
