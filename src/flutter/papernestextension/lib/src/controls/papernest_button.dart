a5ced17b57e76bed117cd3ad9eff6e8555fca789
Drenae <adrien.guilpain@gmail.com>
2026-08-04 04:14:48 +0200

    feat(button): render gradients loading and state animations

--- src/flutter/papernestextension/lib/src/controls/papernest_button.dart
+++ src/flutter/papernestextension/lib/src/controls/papernest_button.dart
@@ -7,6 +7,7 @@ import 'package:flet/src/utils/colors.dart';
 import 'package:flet/src/utils/launch_url.dart';
 import 'package:flet/src/utils/misc.dart';
 import 'package:flet/src/utils/numbers.dart';
+import 'package:flet/src/utils/time.dart';
 import 'package:flet/src/widgets/error.dart';
 import 'package:flet/src/widgets/flet_store_mixin.dart';
 
@@ -26,12 +27,15 @@ class PaperNestButtonControl extends StatefulWidget {
 class _PaperNestButtonControlState extends State<PaperNestButtonControl>
     with FletStoreMixin {
   late final FocusNode _focusNode;
+  late final WidgetStatesController _statesController;
 
   @override
   void initState() {
     super.initState();
     _focusNode = FocusNode();
     _focusNode.addListener(_onFocusChange);
+    _statesController = WidgetStatesController();
+    _statesController.addListener(_onStatesChange);
     widget.control.addInvokeMethodListener(_invokeMethod);
   }
 
@@ -39,6 +43,8 @@ class _PaperNestButtonControlState extends State<PaperNestButtonControl>
   void dispose() {
     _focusNode.removeListener(_onFocusChange);
     _focusNode.dispose();
+    _statesController.removeListener(_onStatesChange);
+    _statesController.dispose();
     widget.control.removeInvokeMethodListener(_invokeMethod);
     super.dispose();
   }
@@ -47,14 +53,69 @@ class _PaperNestButtonControlState extends State<PaperNestButtonControl>
     widget.control.triggerEvent(_focusNode.hasFocus ? "focus" : "blur");
   }
 
+  void _onStatesChange() {
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
   Future<dynamic> _invokeMethod(String name, dynamic args) async {
     debugPrint("PaperNestButton.$name($args)");
     switch (name) {
       case "focus":
         _focusNode.requestFocus();
+        return null;
       default:
         throw Exception("Unknown PaperNestButton method: $name");
     }
+  }
+
+  Set<WidgetState> _resolvedStates({required bool loading}) {
+    final states = Set<WidgetState>.from(_statesController.value);
+    if (widget.control.disabled || loading) {
+      states.add(WidgetState.disabled);
+    }
+    return states;
+  }
+
+  Curve _animationCurve() {
+    return switch (
+        widget.control.getString("animation_curve", "easeOutCubic")) {
+      "linear" => Curves.linear,
+      "easeIn" => Curves.easeIn,
+      "easeOut" => Curves.easeOut,
+      "easeInOut" => Curves.easeInOut,
+      "easeInCubic" => Curves.easeInCubic,
+      "easeInOutCubic" => Curves.easeInOutCubic,
+      _ => Curves.easeOutCubic,
+    };
+  }
+
+  Duration _animationDuration() {
+    return widget.control.getDuration("animation_duration") ??
+        const Duration(milliseconds: 160);
+  }
+
+  double _scaleForStates(Set<WidgetState> states, {required bool enabled}) {
+    if (!enabled) return 1.0;
+    if (states.contains(WidgetState.pressed)) {
+      return widget.control.getDouble("click_scale", 1.0)!;
+    }
+    if (states.contains(WidgetState.hovered)) {
+      return widget.control.getDouble("hover_scale", 1.0)!;
+    }
+    return 1.0;
+  }
+
+  double _offsetYForStates(Set<WidgetState> states, {required bool enabled}) {
+    if (!enabled) return 0.0;
+    if (states.contains(WidgetState.pressed)) {
+      return widget.control.getDouble("click_offset_y", 0)!;
+    }
+    if (states.contains(WidgetState.hovered)) {
+      return widget.control.getDouble("hover_offset_y", 0)!;
+    }
+    return 0.0;
   }
 
   @override
@@ -66,40 +127,16 @@ class _PaperNestButtonControlState extends State<PaperNestButtonControl>
     final isTextButton = widget.control.type == "TextButton";
     final isOutlinedButton = widget.control.type == "OutlinedButton";
 
+    final loading = widget.control.getBool("loading", false)!;
+    final enabled = !widget.control.disabled && !loading;
+    final states = _resolvedStates(loading: loading);
     final url = widget.control.getUrl("url");
-    final iconColor = widget.control.getColor("icon_color", context);
     final clipBehavior =
         widget.control.getClipBehavior("clip_behavior", Clip.none)!;
     final autofocus = widget.control.getBool("autofocus", false)!;
-
-    final icon = widget.control.buildIconOrWidget("icon", color: iconColor);
-    final content = widget.control.buildTextOrWidget("content");
-
-    final Function()? onPressed = !widget.control.disabled
-        ? () {
-            if (url != null) {
-              openWebBrowser(url);
-            }
-            widget.control.triggerEvent("click");
-          }
-        : null;
-
-    final Function()? onLongPressHandler = !widget.control.disabled
-        ? () {
-            widget.control.triggerEvent("long_press");
-          }
-        : null;
-
-    final Function(bool)? onHoverHandler = !widget.control.disabled
-        ? (state) {
-            widget.control.triggerEvent("hover", state);
-          }
-        : null;
-
-    Widget? button;
     final theme = Theme.of(context);
 
-    final style = parsePaperNestButtonStyle(
+    final parsedStyle = parsePaperNestButtonStyle(
       widget.control.internals?["style"] ?? widget.control.get("style"),
       theme,
       defaultForegroundColor: widget.control
@@ -117,6 +154,68 @@ class _PaperNestButtonControlState extends State<PaperNestButtonControl>
           : RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
     );
 
+    final style = parsedStyle?.style;
+    final gradient = parsedStyle?.gradient?.resolve(states);
+    final resolvedShape = style?.shape?.resolve(states) ??
+        (theme.useMaterial3
+            ? const StadiumBorder()
+            : RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)));
+    final resolvedForeground = style?.foregroundColor?.resolve(states) ??
+        widget.control
+            .getColor("color", context, theme.colorScheme.primary)!;
+
+    final iconColor = widget.control.getColor(
+      "icon_color",
+      context,
+      resolvedForeground,
+    );
+    final normalIcon = widget.control.buildIconOrWidget(
+      "icon",
+      color: iconColor,
+    );
+    final loadingIndicator = SizedBox(
+      width: widget.control.getDouble("loading_indicator_size", 16),
+      height: widget.control.getDouble("loading_indicator_size", 16),
+      child: CircularProgressIndicator(
+        strokeWidth:
+            widget.control.getDouble("loading_indicator_stroke_width", 2)!,
+        color: widget.control.getColor(
+          "loading_indicator_color",
+          context,
+          resolvedForeground,
+        ),
+      ),
+    );
+    final icon = loading ? loadingIndicator : normalIcon;
+    final normalContent = widget.control.buildTextOrWidget("content");
+    final loadingText = widget.control.getString("loading_text");
+    final content = loading && loadingText != null
+        ? Text(loadingText)
+        : normalContent;
+
+    final Function()? onPressed = enabled
+        ? () {
+            if (url != null) {
+              openWebBrowser(url);
+            }
+            widget.control.triggerEvent("click");
+          }
+        : null;
+
+    final Function()? onLongPressHandler = enabled
+        ? () {
+            widget.control.triggerEvent("long_press");
+          }
+        : null;
+
+    final Function(bool)? onHoverHandler = enabled
+        ? (state) {
+            widget.control.triggerEvent("hover", state);
+          }
+        : null;
+
+    Widget? button;
+
     const error = ErrorControl(
       "Error displaying PaperNestButton",
       description: "\"icon\" must be specified together with \"content\"",
@@ -128,6 +227,7 @@ class _PaperNestButtonControlState extends State<PaperNestButtonControl>
           style: style,
           autofocus: autofocus,
           focusNode: _focusNode,
+          statesController: _statesController,
           onPressed: onPressed,
           onLongPress: onLongPressHandler,
           onHover: onHoverHandler,
@@ -140,6 +240,7 @@ class _PaperNestButtonControlState extends State<PaperNestButtonControl>
           style: style,
           autofocus: autofocus,
           focusNode: _focusNode,
+          statesController: _statesController,
           onPressed: onPressed,
           onLongPress: onLongPressHandler,
           onHover: onHoverHandler,
@@ -151,6 +252,7 @@ class _PaperNestButtonControlState extends State<PaperNestButtonControl>
         button = TextButton.icon(
           autofocus: autofocus,
           focusNode: _focusNode,
+          statesController: _statesController,
           onPressed: onPressed,
           onLongPress: onLongPressHandler,
           onHover: onHoverHandler,
@@ -163,8 +265,10 @@ class _PaperNestButtonControlState extends State<PaperNestButtonControl>
         button = OutlinedButton.icon(
           autofocus: autofocus,
           focusNode: _focusNode,
+          statesController: _statesController,
           onPressed: onPressed,
           onLongPress: onLongPressHandler,
+          onHover: onHoverHandler,
           clipBehavior: clipBehavior,
           style: style,
           icon: icon,
@@ -175,6 +279,7 @@ class _PaperNestButtonControlState extends State<PaperNestButtonControl>
           style: style,
           autofocus: autofocus,
           focusNode: _focusNode,
+          statesController: _statesController,
           onPressed: onPressed,
           onLongPress: onLongPressHandler,
           onHover: onHoverHandler,
@@ -189,6 +294,7 @@ class _PaperNestButtonControlState extends State<PaperNestButtonControl>
           style: style,
           autofocus: autofocus,
           focusNode: _focusNode,
+          statesController: _statesController,
           onPressed: onPressed,
           onLongPress: onLongPressHandler,
           onHover: onHoverHandler,
@@ -200,6 +306,7 @@ class _PaperNestButtonControlState extends State<PaperNestButtonControl>
           style: style,
           autofocus: autofocus,
           focusNode: _focusNode,
+          statesController: _statesController,
           onPressed: onPressed,
           onLongPress: onLongPressHandler,
           onHover: onHoverHandler,
@@ -210,6 +317,7 @@ class _PaperNestButtonControlState extends State<PaperNestButtonControl>
         button = TextButton(
           autofocus: autofocus,
           focusNode: _focusNode,
+          statesController: _statesController,
           style: style,
           onPressed: onPressed,
           onLongPress: onLongPressHandler,
@@ -221,6 +329,7 @@ class _PaperNestButtonControlState extends State<PaperNestButtonControl>
         button = OutlinedButton(
           autofocus: autofocus,
           focusNode: _focusNode,
+          statesController: _statesController,
           onPressed: onPressed,
           onLongPress: onLongPressHandler,
           clipBehavior: clipBehavior,
@@ -233,6 +342,7 @@ class _PaperNestButtonControlState extends State<PaperNestButtonControl>
           style: style,
           autofocus: autofocus,
           focusNode: _focusNode,
+          statesController: _statesController,
           onPressed: onPressed,
           onLongPress: onLongPressHandler,
           onHover: onHoverHandler,
@@ -242,6 +352,34 @@ class _PaperNestButtonControlState extends State<PaperNestButtonControl>
       }
     }
 
-    return LayoutControl(control: widget.control, child: button);
+    Widget renderedButton = button;
+    if (gradient != null) {
+      renderedButton = AnimatedContainer(
+        duration: _animationDuration(),
+        curve: _animationCurve(),
+        decoration: ShapeDecoration(
+          gradient: gradient,
+          shape: resolvedShape,
+        ),
+        child: button,
+      );
+    }
+
+    renderedButton = AnimatedSlide(
+      offset: Offset(
+        0,
+        _offsetYForStates(states, enabled: enabled) / 40,
+      ),
+      duration: _animationDuration(),
+      curve: _animationCurve(),
+      child: AnimatedScale(
+        scale: _scaleForStates(states, enabled: enabled),
+        duration: _animationDuration(),
+        curve: _animationCurve(),
+        child: renderedButton,
+      ),
+    );
+
+    return LayoutControl(control: widget.control, child: renderedButton);
   }
 }
