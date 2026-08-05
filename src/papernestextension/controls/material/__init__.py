c801ca7d10e6cd355ab073fadc0217a69c09beb4
Drenae <adrien.guilpain@gmail.com>
2026-08-05 07:20:45 +0200

    refactor(alert-dialog): remove variant from material exports

--- src/papernestextension/controls/material/__init__.py
+++ src/papernestextension/controls/material/__init__.py
@@ -1,8 +1,5 @@
 from ..papernest_button_style import PaperNestButtonStyle
-from .papernest_alert_dialog import (
-    PaperNestAlertDialog,
-    PaperNestDialogVariant,
-)
+from .papernest_alert_dialog import PaperNestAlertDialog
 from .papernest_button import PaperNestButton
 from .papernest_color_picker import PaperNestColorPicker
 from .papernest_glide_rail import (
@@ -16,7 +13,6 @@ from .papernest_icon_picker import (
 
 __all__ = [
     "PaperNestAlertDialog",
-    "PaperNestDialogVariant",
     "PaperNestButton",
     "PaperNestButtonStyle",
     "PaperNestColorPicker",
